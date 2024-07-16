cab9c5d4a0b0fac33cc147f9f0a208010cd4325e
ElaineKang <elaine5030331@gmail.com>
2024-07-17 02:38:28 +0800

    Migration(update Account table, Customer table)

---
+++ Infrastructure/Data/Migrations/20240716182139_UpdateSchema.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Customers",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Customers",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
