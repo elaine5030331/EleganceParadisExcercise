25f9aae4fa13f304e5f2eb2bf15d0b7fea181aed
ElaineKang <elaine5030331@gmail.com>
2024-07-19 22:31:45 +0800

    feat : 擴充`OperationResult，差別在於需不需要回傳DTO

--- ApplicationCore/Models/OperationResult.cs
+++ ApplicationCore/Models/OperationResult.cs
@@ -1,25 +1,42 @@
 ﻿namespace ApplicationCore.Models
 {
-    public class OperationResult<T> where T : BaseOperationResult
+    /// <summary>
+    /// 提供service回傳結果及錯誤訊息
+    /// </summary>
+    public class OperationResult
     {
-        public OperationResult() { }
-
+        public OperationResult()
+        {
+            IsSuccess = true;
+        }
         public OperationResult(string errorMessage)
         {
             ErrorMessage = errorMessage;
         }
+
+        public string ErrorMessage { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+
+    /// <summary>
+    /// 提供service回傳結果及錯誤訊息，回傳內容會包含DTO
+    /// </summary>
+    /// <typeparam name="T">`BaseOperationResult`</typeparam>
+    public class OperationResult<T> : OperationResult where T : BaseOperationResult
+    {
+        public OperationResult() : base() { }
+        public OperationResult(string errorMessage) : base(errorMessage) { }
         public OperationResult(T resultDto)
         {
             IsSuccess = true;
             ResultDTO = resultDto;
         }
-        public bool IsSuccess { get; set; }
-        public string ErrorMessage { get; set; }
+
         public T ResultDTO { get; set; }
     }
 
     /// <summary>
-    /// 提供給需要 OperationResult 的 DTO 繼承使用
+    /// 泛型約束：提供給需要 OperationResult 的 DTO 繼承使用
     /// </summary>
     public class BaseOperationResult()
     {
