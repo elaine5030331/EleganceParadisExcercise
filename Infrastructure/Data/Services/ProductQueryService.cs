7a19370315401fdf519daace09554607c085ff9f
ElaineKang <elaine5030331@gmail.com>
2024-07-20 00:02:02 +0800

    fix : `ProductQueryService.GetProductById`：解決Spec及ProductImage重複顯示

--- Infrastructure/Data/Services/ProductQueryService.cs
+++ Infrastructure/Data/Services/ProductQueryService.cs
@@ -56,22 +56,25 @@ namespace Infrastructure.Data.Services
 	                        Specs.SKU AS SKU,
 	                        Specs.UnitPrice AS UnitPrice,
 	                        Specs.SpecName AS SpecName,
-	                        Specs.[Order] AS SpecOrder,
-	                        Specs.StockQuantity AS StockQuantity,
-	                        ProductImages.Id AS ProductImageId,
-	                        ProductImages.URL AS ProductImageUrl,
-	                        ProductImages.[Order] AS ProductImageOrder
+	                        Specs.[Order] AS SpecOrder
                         FROM Products
                         JOIN Specs ON Specs.ProductId = Products.Id
                         JOIN Categories ON Categories.Id = Products.CategoryId
-                        left JOIN ProductImages ON Products.Id = ProductImages.ProductId
-                        WHERE Products.Id = @ProductId";
+                        WHERE Products.Id = @ProductId
+                        AND Products.IsDelete = 0";
 
             var queryResult = (await _connection.QueryAsync<ProductQueryResultDTO>(sql, parameter)).ToList();
-            if (queryResult.Count == 0)
-            {
-                return default;
-            }
+
+            if (queryResult.Count == 0) return null;
+
+            var getProductImagesSQL = @"SELECT 
+	                                        Id AS ProductImageId,
+	                                        URL AS ProductImageUrl,
+	                                        [Order] AS ProductImageOrder
+                                        FROM ProductImages
+                                        WHERE ProductId = @ProductId";
+            var productImagesQueryResult = (await _connection.QueryAsync<ProductImageDTO>(getProductImagesSQL, parameter)).ToList();
+
             var product = queryResult[0];
             var result = new ProductDTO
             {
@@ -89,12 +92,7 @@ namespace Infrastructure.Data.Services
                     SpecOrder = x.SpecOrder,
                     StockQuantity = x.StockQuantity,
                 }).ToList(),
-                ProductImages = queryResult.Select(PI => new ProductImageDTO
-                {
-                    ProductImageId = PI.ProductImageId,
-                    ProductImageUrl = PI.ProductImageUrl,
-                    ProductImageOrder = PI.ProductImageOrder
-                }).ToList()
+                ProductImages = productImagesQueryResult
             };
 
             return result;
