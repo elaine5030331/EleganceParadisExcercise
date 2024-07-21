5ce11dc96635c0402df61fa9077ce9101cddfe36
ElaineKang <elaine5030331@gmail.com>
2024-07-21 22:59:09 +0800

    Migration `Products.CreateAt` and `Specs.CreateAt`

---
+++ Infrastructure/Data/Migrations/20240721145544_addProductFeild_SpecFeild.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductFeild_SpecFeild : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateAt",
+                table: "Specs",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateAt",
+                table: "Products",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Specs");
+
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Products");
+        }
+    }
+}
