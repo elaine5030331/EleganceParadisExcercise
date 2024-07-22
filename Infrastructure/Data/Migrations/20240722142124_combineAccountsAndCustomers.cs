49e6a4a6b6f824f42ae2418f10ce27ee26ef5422
ElaineKang <elaine5030331@gmail.com>
2024-07-22 22:27:07 +0800

    Migration: `Accounts`

---
+++ Infrastructure/Data/Migrations/20240722142124_combineAccountsAndCustomers.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class combineAccountsAndCustomers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Customers",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Customers",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Orders",
+                newName: "AccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                newName: "IX_Orders_AccountId");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerId",
+                table: "Carts",
+                newName: "AccountId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_CustomerId",
+                table: "Carts",
+                newName: "IX_Carts_AccountId");
+
+            migrationBuilder.RenameColumn(
+                name: "Account",
+                table: "Accounts",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "Accounts",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Accounts",
+                table: "Carts",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Accounts",
+                table: "Orders",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Accounts",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Accounts",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "Accounts");
+
+            migrationBuilder.RenameColumn(
+                name: "AccountId",
+                table: "Orders",
+                newName: "CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_AccountId",
+                table: "Orders",
+                newName: "IX_Orders_CustomerId");
+
+            migrationBuilder.RenameColumn(
+                name: "AccountId",
+                table: "Carts",
+                newName: "CustomerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_AccountId",
+                table: "Carts",
+                newName: "IX_Carts_CustomerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Accounts",
+                newName: "Account");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_Accounts",
+                        column: x => x.Id,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Customers",
+                table: "Carts",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Customers",
+                table: "Orders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
