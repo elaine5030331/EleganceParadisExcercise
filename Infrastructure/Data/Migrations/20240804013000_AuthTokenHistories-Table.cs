fdd4791849ea4fded8e6090d442df0c066d4d5cc
ElaineKang <elaine5030331@gmail.com>
2024-08-04 09:33:21 +0800

    Migration: `AuthTokenHistories`

---
+++ Infrastructure/Data/Migrations/20240804013000_AuthTokenHistories-Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthTokenHistoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthTokenHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    AccessToken = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "JWT"),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpiredTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false, comment: "RefreshToken過期時間"),
+                    CreatAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthTokenHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuthTokenHistories_Accounts",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthTokenHistories_AccountId",
+                table: "AuthTokenHistories",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthTokenHistories");
+        }
+    }
+}
