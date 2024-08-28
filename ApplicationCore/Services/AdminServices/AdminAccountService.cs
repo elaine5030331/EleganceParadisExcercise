25d69bfd9d6ce7f67e3665975b65d80cfc475053
ElaineKang <elaine5030331@gmail.com>
2024-08-28 16:38:12 +0800

    feat: add API /api/AdminAccount/GetAccountById 1. add `IAdminAccountService` and Implement(`AdminAccountService`) 2. add `AdminAccountController`

---
+++ ApplicationCore/Services/AdminServices/AdminAccountService.cs
@@ -0,0 +1,56 @@
+﻿using ApplicationCore.DTOs.AdminDTOs.AccountDTOs;
+using ApplicationCore.Entities;
+using ApplicationCore.Enums;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Interfaces.AdminInterfaces;
+using ApplicationCore.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services.AdminServices
+{
+    public class AdminAccountService : IAdminAccountService
+    {
+        private readonly IRepository<Account> _accountRepo;
+        private readonly ILogger<AdminAccountService> _logger;
+
+        public AdminAccountService(IRepository<Account> accountRepo, ILogger<AdminAccountService> logger)
+        {
+            _accountRepo = accountRepo;
+            _logger = logger;
+        }
+
+        public async Task<OperationResult<GetAccountByIdResponse>> GetAccountByIdAsync(int accountId)
+        {
+            try
+            {
+                var account = await _accountRepo.GetByIdAsync(accountId);
+                if(account == null) 
+                    return new OperationResult<GetAccountByIdResponse>("找不到對應的AccountId");
+
+                return new OperationResult<GetAccountByIdResponse>()
+                {
+                    IsSuccess = true,
+                    ResultDTO = new GetAccountByIdResponse()
+                    {
+                        AccountId = accountId,
+                        AccountName = account.Name,
+                        Email = account.Email,
+                        Mobile = account.Mobile,
+                        CreateAt = account.CreateAt.ToLocalTime().ToString("yyyy/MM/dd"),
+                        Status = (int)account.Status
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new OperationResult<GetAccountByIdResponse>("取得會員資料失敗");
+            }
+        }
+    }
+}
---
+++ EleganceParadisAPI/AdminControllers/AdminAccountController.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationCore.Constants;
+using ApplicationCore.Interfaces.AdminInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EleganceParadisAPI.AdminControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = EleganceParadisRole.Admin)]
+    public class AdminAccountController : ControllerBase
+    {
+        private readonly IAdminAccountService _adminAccountService;
+
+        public AdminAccountController(IAdminAccountService adminAccountService)
+        {
+            _adminAccountService = adminAccountService;
+        }
+
+        /// <summary>
+        /// 取得會員資料by accountId
+        /// </summary>
+        /// <param name="accountId"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample response:<br/>
+        ///     {
+        ///         "accountId": 2,
+        ///         "email": "test@gmail.com",
+        ///         "accountName": "test1",
+        ///         "mobile": "0912345678",
+        ///         "createAt": "2024/07/22",
+        ///         "status": 2   //Unverified = 1, Verified = 2, Blacklist = 3
+        ///     }
+        /// </remarks>
+        /// <response code ="200">成功取得 accountId 的會員資料</response>
+        /// <response code ="400">
+        /// 1. 找不到對應的AccountId
+        /// 2. 取得會員資料失敗
+        /// </response>
+        [HttpGet("GetAccountById")]
+        public async Task<IActionResult> GetAccountById(int accountId)
+        {
+            var result = await _adminAccountService.GetAccountByIdAsync(accountId);
+            if(result.IsSuccess) return Ok(result.ResultDTO);
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+}
