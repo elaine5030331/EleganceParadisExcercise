6fb1ebef04afda1cc96087231e12de37f8d93974
ElaineKang <elaine5030331@gmail.com>
2024-08-29 20:41:24 +0800

    feat: 後台Spec相關API移至`AdminControllers`資料夾內 1. 新增`AdminSpecController`

--- ApplicationCore/Services/SpecService.cs
+++ ApplicationCore/Services/SpecService.cs
@@ -37,43 +37,46 @@ namespace ApplicationCore.Services
             catch (Exception ex) 
             {
                 _logger.LogError(ex, ex.Message);
-                return new OperationResult("產品規格新增失敗");
+                return new OperationResult("商品規格新增失敗");
             }
         }
 
         public async Task<OperationResult> UpdateSpecAsync(UpdateSpecDTO updateSpecDTO)
         {
-            var spec = await _specRepository.GetByIdAsync(updateSpecDTO.SpecId);
-
-            spec.Sku = updateSpecDTO.SKU;
-            spec.UnitPrice = updateSpecDTO.UnitPrice;
-            spec.SpecName = updateSpecDTO.SpecName;
-            spec.Order = updateSpecDTO.Order;
-            spec.StockQuantity = updateSpecDTO.StockQuantity;
-
             try
             {
+                var spec = await _specRepository.GetByIdAsync(updateSpecDTO.SpecId);
+                if (spec == null) return new OperationResult("找不到對應的商品規格");
+
+                spec.Sku = updateSpecDTO.SKU;
+                spec.UnitPrice = updateSpecDTO.UnitPrice;
+                spec.SpecName = updateSpecDTO.SpecName;
+                spec.Order = updateSpecDTO.Order;
+                spec.StockQuantity = updateSpecDTO.StockQuantity;
+
                 await _specRepository.UpdateAsync(spec);
                 return new OperationResult();
             }
             catch(Exception ex)
             {
-                return new OperationResult("產品規格更新失敗");
+                return new OperationResult("商品規格更新失敗");
             }
         }
 
         public async Task<OperationResult> DeleteSpecAsync(int specId)
         {
-            var spec = await _specRepository.GetByIdAsync(specId);
             try
             {
+                var spec = await _specRepository.GetByIdAsync(specId);
+                if (spec == null) return new OperationResult("找不到對應的商品規格");
+
                 await _specRepository.DeleteAsync(spec);
                 return new OperationResult();
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex,ex.Message);
-                return new OperationResult("產品規格刪除失敗");
+                return new OperationResult("商品規格刪除失敗");
             }
         }
     }
--- EleganceParadisAPI/Controllers/SpecController.cs
+++ EleganceParadisAPI/Controllers/SpecController.cs
@@ -11,7 +11,6 @@ namespace EleganceParadisAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = EleganceParadisRole.Admin)]
     public class SpecController : ControllerBase
     {
         private readonly ISpecService _specService;
@@ -19,63 +18,6 @@ namespace EleganceParadisAPI.Controllers
         public SpecController(ISpecService specService)
         {
             _specService = specService;
-        }
-
-        /// <summary>
-        /// 新增產品規格
-        /// </summary>
-        /// <param name="specDTO"></param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Sample request:<br/>
-        ///     {
-        ///        "productId": 產品ID
-        ///        "sku": 產品類別 + 產品名稱 + 產品規格
-        ///        "unitPrice": 單價
-        ///        "specName": 產品規格名稱(ex：50ml, 75ml, 1 peice)
-        ///        "stockQuantity": 庫存量
-        ///     }
-        /// </remarks>
-        /// <response code ="201">產品規格新增成功</response>
-        /// <response code ="400">產品規格新增失敗</response>
-        [HttpPost("AddSpec")]
-        public async Task<IActionResult> AddSpec(AddSpecDTO specDTO)
-        {
-            var result = await _specService.AddSpecAsync(specDTO);
-            if (result.IsSuccess) return Created();
-            return BadRequest(result.ErrorMessage);
-        }
-
-        /// <summary>
-        /// 更新產品規格
-        /// </summary>
-        /// <param name="specId"></param>
-        /// <param name="specDTO"></param>
-        /// <returns></returns>
-        /// <response code ="204">產品規格新增成功</response>
-        /// <response code ="400">產品規格更新失敗</response>
-        [HttpPut("UpdateSpec/{specId}")]
-        public async Task<IActionResult> UpdateSpec(int specId, UpdateSpecDTO specDTO)
-        {
-            if (specId != specDTO.SpecId) return BadRequest();
-            var result = await _specService.UpdateSpecAsync(specDTO);
-            if(result.IsSuccess) return NoContent();
-            return BadRequest(result.ErrorMessage);
-        }
-
-        /// <summary>
-        /// 刪除產品規格
-        /// </summary>
-        /// <param name="specId"></param>
-        /// <returns></returns>
-        /// <response code ="204">刪除成功</response>
-        /// <response code ="400">刪除失敗</response>
-        [HttpDelete("DeleteSpec/{specId}")]
-        public async Task<IActionResult> DeleteSpec(int specId)
-        {
-            var result = await _specService.DeleteSpecAsync(specId);
-            if (result.IsSuccess) return NoContent();
-            return BadRequest(result.ErrorMessage);
         }
     }
 }
